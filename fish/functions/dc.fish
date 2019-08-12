ed9a2c1d2ce85f66c5295ec8007ed6d0cc06c599
Sophia <tech@s-br.net>
2019-08-12 16:20:01 +0200

    📦 NEW: add aliases for docker-compose & docker-machine

---
+++ fish/functions/dc.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function dc --description 'alias dc docker-compose'
+	docker-compose  $argv;
+end
---
+++ fish/functions/dm.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function dm --description 'alias dm docker-machine'
+	docker-machine  $argv;
+end
