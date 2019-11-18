5462fda789475b95d14ba8d79aac66a559ee12e1
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-11-18 08:07:05 +0100

    📦 NEW: add bass, add asdf

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,8 @@ set -g -x fish_greeting ''
 # fnm for node management
 fnm env --multi | source
 
-# asdf for elixir/erlang
-source ~/.asdf/asdf.fish
-
 # opam configuration
 source /home/owlhoot/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# asdf
+source ~/.asdf/asdf.fish
