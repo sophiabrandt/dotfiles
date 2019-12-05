ebb3fc74fd88114667eb72d239106c4aa4fa67cc
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-12-05 06:51:44 +0100

    📦 NEW: add virtualfish

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,6 @@ source /home/owlhoot/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
 # asdf
 source ~/.asdf/asdf.fish
+
+# virtualfish
+eval (python -m virtualfish compat_aliases auto_activation)
