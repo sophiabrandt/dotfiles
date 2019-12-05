fd6cd2cb3f1d40dda208a79e1147cbc5ad1120d2
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-12-05 06:59:07 +0100

    👌 IMPROVE: add virtualenv to fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -25,5 +25,9 @@ function fish_prompt
     end
   end
 
+  if set -q VIRTUAL_ENV
+      echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+  end
+
   echo -n -s $cwd $git_info $normal ' ' $arrow ' '
 end
