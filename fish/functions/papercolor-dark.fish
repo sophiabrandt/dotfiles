10bb68282e2f3f7139af9d85cc565e6e04853608
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-02-04 13:31:41 +0100

    👌 IMPROVE: switch default configured kitty colors, too

--- fish/functions/papercolor-dark.fish
+++ fish/functions/papercolor-dark.fish
@@ -1,4 +1,4 @@
 function papercolor-dark
-	kitty @ set-colors --all ~/.config/kitty/papercolor-kitty/papercolor-dark.conf
+	kitty @ set-colors -a -c ~/.config/kitty/papercolor-kitty/papercolor-dark.conf
 sed -e '1 c\colorscheme PaperColor' -e '2 c\let g:lightline.colorscheme="PaperColor"' -e '3d' ~/.config/nvim/colorscheme.vim > ~/.config/nvim/colorscheme.vim.tmp && mv ~/.config/nvim/colorscheme.vim.tmp ~/.config/nvim/colorscheme.vim
 end
