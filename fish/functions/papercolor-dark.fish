654e6b4955b6fc95e65310528c504ab98bbfa232
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-02-12 17:13:39 +0100

    🐛 FIX: fix color changer functions

--- fish/functions/papercolor-dark.fish
+++ fish/functions/papercolor-dark.fish
@@ -1,4 +1,4 @@
 function papercolor-dark
 	kitty @ set-colors -a -c ~/.config/kitty/papercolor-kitty/papercolor-dark.conf
-sed -e '1 c\colorscheme PaperColor' -e '2 c\let g:lightline.colorscheme="PaperColor"' -e '3d' ~/.config/nvim/colorscheme.vim > ~/.config/nvim/colorscheme.vim.tmp && mv ~/.config/nvim/colorscheme.vim.tmp ~/.config/nvim/colorscheme.vim
+sed -e '1 c\colorscheme PaperColor' -e '2 c\let g:lightline.colorscheme="PaperColor"' -e '3 c\ ' ~/.config/nvim/colorscheme.vim > ~/.config/nvim/colorscheme.vim.tmp && mv ~/.config/nvim/colorscheme.vim.tmp ~/.config/nvim/colorscheme.vim
 end
--- fish/functions/papercolor-light.fish
+++ fish/functions/papercolor-light.fish
@@ -1,4 +1,4 @@
 function papercolor-light
 	kitty @ set-colors -a -c ~/.config/kitty/papercolor-kitty/papercolor-light.conf
-sed -e '1 c\colorscheme PaperColor' -e '2a set background=light' -e '2 c\let g:lightline.colorscheme="PaperColor"' ~/.config/nvim/colorscheme.vim > ~/.config/nvim/colorscheme.vim.tmp && mv ~/.config/nvim/colorscheme.vim.tmp ~/.config/nvim/colorscheme.vim
+sed -e '1 c\colorscheme PaperColor' -e '2 c\let g:lightline.colorscheme="PaperColor"' -e '3 c\set background=light' ~/.config/nvim/colorscheme.vim > ~/.config/nvim/colorscheme.vim.tmp && mv ~/.config/nvim/colorscheme.vim.tmp ~/.config/nvim/colorscheme.vim
 end
