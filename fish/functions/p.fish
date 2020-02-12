219fbac0ab69297de545a545979f80f11f3ed151
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-02-12 17:16:08 +0100

    👌 IMPROVE: use abbreviations instead of aliases

--- fish/functions/p.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function p --description 'alias p pnpm'
-	pnpm  $argv;
-end
--- fish/functions/px.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function px --description 'alias px pnpx'
-	pnpx  $argv;
-end
