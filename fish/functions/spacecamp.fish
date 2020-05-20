d54c11f5b366ac4da887ad41c3255717d1d82c44
Sophia Brandt <git@s-br.net>
2020-05-20 18:02:18 +0200

    🐛 FIX: fix spacecamp function (switch colorscheme)

--- fish/functions/spacecamp.fish
+++ fish/functions/spacecamp.fish
@@ -1,5 +1,5 @@
-# Defined in /home/owlhoot/.config/fish/functions/spacecamp.fish @ line 1
+# Defined in /home/owlhoot/.config/fish/functions/spacecamp.fish @ line 2
 function spacecamp
-	kitty @ set-colors -a -c ~/.config/kitty/spacecamp.conf
+	kitty @ set-colors -a -c ~/.config/kitty/spacecamp-kitty/spacecamp.conf
 sed -e '1 c\colorscheme spacecamp' -e '2 c\let g:lightline.colorscheme="wombat"' -e '3 c\ ' ~/.config/nvim/colorscheme.vim > ~/.config/nvim/colorscheme.vim.tmp && mv ~/.config/nvim/colorscheme.vim.tmp ~/.config/nvim/colorscheme.vim
 end
