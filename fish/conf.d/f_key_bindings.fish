a84240b467ee83d3fb23794bbdb8ef7860598880
Sophia Brandt <git@s-br.net>
2020-07-14 17:35:13 +0200

    🐛 FIX: update f key bindings

--- fish/conf.d/f_key_bindings.fish
+++ fish/conf.d/f_key_bindings.fish
@@ -8,18 +8,18 @@ function __f_insert_from_picker
     commandline -f repaint
 end
 
-# if not bind \ek
-#     bind \ek __f_insert_from_picker
-# end
+if not bind \ek 2>/dev/null
+    bind \ek __f_insert_from_picker
+end
 
-# if not bind -M insert \ek
-#     bind -M insert \ek __f_insert_from_picker
-# end
+if not bind -M insert \ek 2>/dev/null
+    bind -M insert \ek __f_insert_from_picker
+end
 
-# function f_key_bindings_uninstall --on-event f_key_bindings_uninstall
-#     bind \ek | string match -e __f_insert_from_picker
-#     and bind -e \ek
+function f_key_bindings_uninstall --on-event f_key_bindings_uninstall
+    bind \ek 2>/dev/null | string match -e __f_insert_from_picker
+    and bind -e \ek
 
-#     bind -M insert \ek | string match -e __f_insert_from_picker
-#     and bind -M insert -e \ek
-# end
+    bind -M insert \ek 2>/dev/null | string match -e __f_insert_from_picker
+    and bind -M insert -e \ek
+end
