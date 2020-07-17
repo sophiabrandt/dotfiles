52487ec7a5ff29078cbd20362ee6dee7d28e1eb7
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-07-17 13:16:29 +0200

    👌 IMPROVE: rollback to sashimi, but add virtualenv prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,70 +1,77 @@
-# fish theme: edan
-# Display the following bits on the left:
-# * User & host (hidden by default, execute `edan-remote`to show, `edan-local` to hide)
-# * Virtualenv name (if applicable, see https://github.com/adambrenecki/virtualfish)
-# * Current directory name
-# * Git branch and dirty state (if inside a git repo)
-
-function _git_branch_name
-  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
-end
-
-function _user_host
-  if [ (id -u) = "0" ];
-    echo -n (set_color -o red)
-  else
-    echo -n (set_color -o blue)
-  end
-  echo -n (hostname|cut -d . -f 1)ˇ$USER (set color normal)
-end
-
+# Defined in /home/owlhoot/.config/fish/functions/fish_prompt.fish @ line 2
 function fish_prompt
-  set fish_greeting
+  function _git_ahead
+    set -l commits (command git rev-list --left-right '@{upstream}...HEAD' ^/dev/null)
+    if [ $status != 0 ]
+      return
+    end
+    set -l behind (count (for arg in $commits; echo $arg; end | grep '^<'))
+    set -l ahead  (count (for arg in $commits; echo $arg; end | grep -v '^<'))
+    switch "$ahead $behind"
+      case ''     # no upstream
+      case '0 0'  # equal to upstream
+        return
+      case '* 0'  # ahead of upstream
+        echo "$blue↑$normal_c$ahead$whitespace"
+      case '0 *'  # behind upstream
+        echo "$red↓$normal_c$behind$whitespace"
+      case '*'    # diverged from upstream
+        echo "$blue↑$normal$ahead $red↓$normal_c$behind$whitespace"
+    end
+  end
+
+  function _git_branch_name
+    echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+  end
+
+  function _is_git_dirty
+    echo (command git status -s --ignore-submodules=dirty ^/dev/null)
+  end
+  set -l last_status $status
   set -l cyan (set_color -o cyan)
   set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
+  set -g red (set_color -o red)
+  set -g blue (set_color -o blue)
   set -l green (set_color -o green)
-  set -l normal (set_color normal)
+  set -g normal (set_color normal)
 
-  set -l cwd $cyan(basename (prompt_pwd))
-
-  # output the prompt, left to right
-
-  # Add a newline before prompts
-  echo -e ""
+  set -l ahead (_git_ahead)
+  set -g whitespace ' '
 
   # Display [venvname] if in a virtualenv
   if set -q VIRTUAL_ENV
-    echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
+    echo -n -s (set_color -b yellow black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
   end
 
-  # Display [user & host] when on remote host
-  if [ "$EDAN_HOST_TYPE" = "remote" ]
-    _user_host; echo -n ': '
+  if test $last_status = 0
+    set initial_indicator "$green◆"
+    set status_indicator "$normal❯$cyan❯$green❯"
+  else
+    set initial_indicator "$red✖ $last_status"
+    set status_indicator "$red❯$red❯$red❯"
   end
+  set -l cwd $cyan(basename (prompt_pwd))
 
-  # Display the current directory name
-  echo -n -s $cwd $normal
-
-
-  # Show git branch and dirty state
   if [ (_git_branch_name) ]
-    set -l git_branch '[' (_git_branch_name) ']'
+
+    if test (_git_branch_name) = 'master'
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($red$git_branch$normal)"
+    else
+      set -l git_branch (_git_branch_name)
+      set git_info "$normal git:($blue$git_branch$normal)"
+    end
 
     if [ (_is_git_dirty) ]
-      set git_info $red $git_branch "×"
-    else
-      set git_info $green $git_branch
+      set -l dirty "$yellow ✗"
+      set git_info "$git_info$dirty"
     end
-    echo -n -s ' ' $git_info $normal
   end
 
-  # Terminate with a nice prompt char
-  echo -n -s ' » ' $normal
+  # Notify if a command took more than 5 minutes
+  if [ "$CMD_DURATION" -gt 300000 ]
+    echo The last command took (math "$CMD_DURATION/1000") seconds.
+  end
 
+  echo -n -s $initial_indicator $whitespace $cwd $git_info $whitespace $ahead $status_indicator $whitespace
 end
