a4199e58528fc6ed9149b164170b32bea504d0cf
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-07-17 17:11:47 +0200

    👌 IMPROVE: delete functions, setup abbreviations

--- fish/functions/fzfd.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function fzfd --description 'alias fzfd fd | fzy'
-	fd | fzy $argv;
-end
--- fish/functions/fzfv.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function fzfv --description 'alias fzfv nvim (fd | fzy)'
-	nvim (fd | fzy) $argv;
-end
