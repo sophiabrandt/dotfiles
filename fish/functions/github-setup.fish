facaa94d1abde0e8634442f3d9cd40b270de66d4
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-07-17 11:49:43 +0200

    📦 NEW: add git setup functions for gitlab & github

---
+++ fish/functions/github-setup.fish
@@ -0,0 +1,8 @@
+# Defined in /tmp/fish.xi9WMk/github-setup.fish @ line 2
+function github-setup
+    git init
+    git config user.name 'Sophia Brandt'
+    git config committer.name 'Sophia Brandt'
+    git config user.email '16630701+sophiabrandt@users.noreply.github.com'
+    git config committer.email '16630701+sophiabrandt@users.noreply.github.com'
+end
---
+++ fish/functions/gitlab-setup.fish
@@ -0,0 +1,8 @@
+# Defined in /tmp/fish.wS2fmZ/gitlab-setup.fish @ line 2
+function gitlab-setup
+    git init
+    git config user.name 'Sophia Brandt'
+    git config committer.name 'Sophia Brandt'
+    git config user.email '1578703-sophiabrandt@users.noreply.gitlab.com'
+    git config committer.email '1578703-sophiabrandt@users.noreply.gitlab.com'
+end
