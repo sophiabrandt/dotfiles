ca07cdf05d62568dfcfb56b3a1dba73788cd522c
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-08-07 09:50:31 +0200

    📦 NEW: Add new themes (fahrenhet/oceanicnext)

---
+++ fish/functions/fahrenheit.fish
@@ -0,0 +1,4 @@
+function fahrenheit
+	kitty @ set-colors -a -c ~/.config/kitty/kitty-themes/themes/IC_Orange_PPL.conf
+sed -e '1 c\colorscheme fahrenheit' -e '2 c\let g:lightline.colorscheme="powerline"' -e '3 c\ ' ~/.config/nvim/colorscheme.vim > ~/.config/nvim/colorscheme.vim.tmp && mv ~/.config/nvim/colorscheme.vim.tmp ~/.config/nvim/colorscheme.vim
+end
