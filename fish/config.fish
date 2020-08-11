b7a6b72da27f5748437cfa4f81f13e9c9a6ab093
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-08-11 06:32:39 +0200

    👌 IMPROVE: Remove z, add zoixide

--- fish/config.fish
+++ fish/config.fish
@@ -9,6 +9,9 @@ source /home/owlhoot/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 # asdf
 source ~/.asdf/asdf.fish
 
+# zoxide
+zoxide init fish | source
+
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
