f1f215c55427f249c1e72f0a4c75e7b793d2360a
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-10-28 09:15:36 +0100

    👌 IMPROVE: Update fnm

---
+++ fish/conf.d/fnm.fish
@@ -0,0 +1,3 @@
+# fnm
+set PATH /home/owlhoot/.fnm $PATH
+fnm env | source
--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set -g -x fish_greeting ''
 
 # fnm for node management
-fnm env --multi | source
+fnm env | source
 
 # opam configuration
 source /home/owlhoot/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
