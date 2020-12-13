500942ffe14a9ca8cba6e4968948e8f9743cac8e
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2020-12-13 12:56:59 +0100

    👌 IMPROVE: Update deps

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -3,6 +3,5 @@ set -g VIRTUALFISH_PYTHON_EXEC /home/owlhoot/.local/pipx/venvs/virtualfish/bin/p
 source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.8/site-packages/virtualfish/virtual.fish
 source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.8/site-packages/virtualfish/auto_activation.fish
 source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.8/site-packages/virtualfish/compat_aliases.fish
-source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.8/site-packages/virtualfish/projects.fish
 source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.8/site-packages/virtualfish/environment.fish
 emit virtualfish_did_setup_plugins
