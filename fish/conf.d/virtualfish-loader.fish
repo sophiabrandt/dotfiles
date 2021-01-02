e43fe4aea4b845ea18541a968f58f85234bbd98f
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-01-02 21:19:08 +0100

    👌 IMPROVE: Update fish

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,7 +1,7 @@
-set -g VIRTUALFISH_VERSION 2.3.0
+set -g VIRTUALFISH_VERSION 2.5.1
 set -g VIRTUALFISH_PYTHON_EXEC /home/owlhoot/.local/pipx/venvs/virtualfish/bin/python
-source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.8/site-packages/virtualfish/virtual.fish
-source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.8/site-packages/virtualfish/auto_activation.fish
-source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.8/site-packages/virtualfish/compat_aliases.fish
-source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.8/site-packages/virtualfish/environment.fish
+source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.9/site-packages/virtualfish/virtual.fish
+source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.9/site-packages/virtualfish/compat_aliases.fish
+source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.9/site-packages/virtualfish/environment.fish
+source /home/owlhoot/.local/pipx/venvs/virtualfish/lib/python3.9/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins
--- fish/functions/yup.fish
+++
@@ -1,3 +0,0 @@
-function yup
-	yarn upgrade && npx syncyarnlock -s -k && yarn install
-end
