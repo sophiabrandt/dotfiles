e645053d44638f53d7554a06cd6b01c7f0c23b7f
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-01-20 17:23:28 +0100

    📦 NEW: Alias jql
    This is necessary because both the rust binary and the Go binary have the same name.

---
+++ fish/functions/jqg.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function jqg --wraps=/home/owlhoot/.go/bin/jql --description 'alias jqg=/home/owlhoot/.go/bin/jql'
+  /home/owlhoot/.go/bin/jql  $argv;
+end
---
+++ fish/functions/jqr.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function jqr --wraps=/home/owlhoot/.cargo/bin/jql --description 'alias jqr=/home/owlhoot/.cargo/bin/jql'
+  /home/owlhoot/.cargo/bin/jql  $argv;
+end
