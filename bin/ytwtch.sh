c9d0e7d3046010cece3b2e52cc07e6b8fb7b1237
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-01-20 17:24:10 +0100

    📦 NEW: Add youtube watch script with mpv

--- bin/ytwtch.sh
+++ bin/ytwtch.sh
@@ -2,5 +2,5 @@
 # by Kris Occhipinti
 # https://www.youtube.com/watch?v=FsQuGplQvrw
 
-notify-send -t 3000 --icon=video-televison "Playing Video" "$(xclip -o)";
-mpv --ytdl-format=22 --fs "$(xclip -o)" || mpv --fs "$(xclip -o)"
+notify-send -t 3000 "Playing Video" "$(xclip -o)";
+mpv --ytdl-format=bestvideo+bestaudio/best --fs --speed=2.5 --af=rubberband=pitch-scale=0.981818181818181 "$(xclip -o)"
