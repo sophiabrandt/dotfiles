86c4228db000574b848a99ea4ab9d0ce0ec5e757
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-03-15 08:58:24 +0100

    📦 NEW: Add posix-source fish plugin, update universal variables

---
+++ fish/completions/posix-source.fish
@@ -0,0 +1,7 @@
+complete --command=posix-source --description='Source env files'
+complete --command=posix-source --short-option=h --long-option=help --description='Print help file'
+complete --command=posix-source --short-option=n --long-option=no-interpret --description='Don’t evaluate variables in env files'
+complete --command=posix-source --short-option=p --long-option=print --description='Print fish commands to STDOUT instead of setting varaibles'
+complete --command=posix-source --short-option=G --long-option=no-global --description='Unset variable at end of block'
+complete --command=posix-source --short-option=u --long-option=unexport --description='Don’t export variable to child processes'
+complete --command=posix-source --short-option=e --long-option=erase --description='Erase variables listed in file'
--- fish/functions/posix-source.fish
+++ fish/functions/posix-source.fish
@@ -1,11 +1,84 @@
-# Defined in /home/owlhoot/.config/fish/functions/posix-source.fish @ line 1
-function posix-source
-	for i in (cat $argv)
-		if test (echo $i | sed -E 's/^[[:space:]]*(.).+$/\\1/g') != "#"
-			set arr (echo $i |tr = \n)
-			if test $arr[1] != ""
-				set -gx $arr[1] $arr[2]
-			end
-		end
-	end
+function posix-source --description='Source env files'
+  set -l options \
+    (fish_opt --short=h --long=help) \
+    (fish_opt --short=p --long=print) \
+    (fish_opt --short=n --long=no-interpret) \
+    (fish_opt --short=G --long=no-global) \
+    (fish_opt --short=u --long=unexport) \
+    (fish_opt --short=e --long=erase)
+  argparse $options -- $argv
+
+  [ (count $argv ) -eq 0 ]; and set _flag_help
+
+  if set -q _flag_help
+    set fn (status function)
+    echo \
+      "USAGE:
+
+      	$fn ( --help | -h )
+      		This help message
+
+      	$fn [OPTIONS] FILE ...
+      		Source one or more env files
+
+
+      OPTIONS:
+
+      	-p, --print
+      		Print the commands to STDOUT
+
+      	-G, --no-global
+      		By default, variables will be set globally. This flag causes
+      		variables to be unset at the end of the block.
+
+      	-u, --unexport
+      		By default, variables will be exported to child processes.
+      		This flag prevents that.
+
+      	-n, --no-interpret
+      		By default, variables in env files will be interpreted. Pass
+      		this argument to interpret dollar signs as literal characters.
+
+      	-e, --erase
+      		Erase all variables listed in the given file(s)." \
+    | string replace --all --regex '(^ +)' ''
+    return 0
+  end
+
+  set -q _flag_no_global
+  or set setFlags -g
+
+  set -q _flag_unexport
+  or set setFlags -x $setFlags
+
+
+  for file in $argv
+    not [ -f $file ]
+    and echo "$file is not a file!" >&2
+    and continue
+
+    for instruction in (cat $file)
+      set arr (string split -m1 = $instruction)
+
+      set -q _flag_erase
+      and set -q _flag_print
+      and echo set -e $arr[1]\;
+      and continue
+
+      if set -q _flag_erase
+        set -e $arr[1]
+        continue
+      end
+
+      set -q _flag_print
+      and echo set $setFlags $arr[1] $arr[2]\;
+      and continue
+
+      set -q _flag_no_interpret
+      and set $setFlags $arr[1] $arr[2]
+      or eval "set $setFlags $arr[1] $arr[2]"
+    end
+  end
+
+  return 0
 end
