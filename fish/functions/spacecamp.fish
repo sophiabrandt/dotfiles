2338ee3af9c1d3d4d9d9ff7dfdb2bbcf1dc5d458
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-05-17 17:53:30 +0200

    🐛 FIX: spacecamp function

--- fish/functions/spacecamp.fish
+++ fish/functions/spacecamp.fish
@@ -1,5 +1,5 @@
 # Defined in /home/owlhoot/.config/fish/functions/spacecamp.fish @ line 2
 function spacecamp
 	kitty @ set-colors -a -c ~/.config/kitty/spacecamp.conf
-	sed -e '1 c\colorscheme spacecamp' ~/.vim/custom/colorscheme.vim > ~/.vim/custom/colorscheme.vim.tmp && mv ~/.vim/custom/colorscheme.vim.tmp ~/.vim/custom/colorscheme.vim
+	echo 'colorscheme spacecamp' > ~/.vim/custom/colorscheme.vim
 end
