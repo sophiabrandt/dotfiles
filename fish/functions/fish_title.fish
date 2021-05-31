57229dabbb7ae02321ec6b005f53e33e1d010aa5
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-05-31 11:10:51 +0200

    📦 NEW: Add - abbreviation
    Expands to `cd -`.

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,4 +1,4 @@
-function fish_title --argument-names last_cmd
-    string replace --all --regex -- \s\*\n "; " $last_cmd
-    string replace ~ " ~" $PWD
+# Defined interactively
+function fish_title
+ 
 end
