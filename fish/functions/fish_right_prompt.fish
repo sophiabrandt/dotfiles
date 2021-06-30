12ca8b41755f2b198efa4fa977b2aa59fb9cce68
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-06-30 15:46:57 +0200

    👌 IMPROVE: Move notes

--- fish/functions/fish_right_prompt.fish
+++
@@ -1,6 +0,0 @@
-function fish_right_prompt -d "Display [venvname] if in a virtualenv"
-    if set -q VIRTUAL_ENV
-      echo -n -s (set_color -b magenta black) '[' (basename "$VIRTUAL_ENV") ']'
-    end
-    set_color normal
-end
