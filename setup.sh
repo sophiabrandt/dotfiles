b37d607147ea088a050cc0c5c104c0fae9c279e8
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-07-01 19:35:24 +0200

    📦 NEW: global gitignore file

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 cd ~
-ln -s dotfiles/bash/bash_profile .bash_profile
-ln -s dotfiles/bash/bashrc       .bashrc
-ln -s dotfiles/bash/profile      .profile
-ln -s dotfiles/fish              .config/fish
-ln -s dotfiles/git/gitconfig     .gitconfig
-ln -s dotfiles/tmux/tmux.conf    .tmux.conf
-ln -s dotfiles/kitty             .config/kitty
-ln -s dotfiles/vim/              .vim
-ln -s dotfiles/vimrc             .vimrc
+ln -s dotfiles/bash/bash_profile    .bash_profile
+ln -s dotfiles/bash/bashrc          .bashrc
+ln -s dotfiles/bash/profile         .profile
+ln -s dotfiles/fish                 .config/fish
+ln -s dotfiles/git/gitconfig        .gitconfig
+ln -s dotfiles/git/gitignore_global .gitconfig
+ln -s dotfiles/tmux/tmux.conf       .tmux.conf
+ln -s dotfiles/kitty                .config/kitty
+ln -s dotfiles/vim/                 .vim
+ln -s dotfiles/vimrc                .vimrc
 
 mkdir -p .config/nvim
 ln -s ~/dotfiles/nvim/init.vim .config/nvim/init.vim
