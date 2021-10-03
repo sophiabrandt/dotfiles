a0e9170dfe268730bb8d2f4c26bc637d72aa4f4b
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2021-10-03 08:39:15 +0200

    👌 IMPROVE: Use -15 to clean up

--- fish/functions/sptlauncher.fish
+++ fish/functions/sptlauncher.fish
@@ -1,6 +1,6 @@
 # Defined in $HOME/.config/fish/functions/sptlauncher.fish @ line 1
 function sptlauncher
-	kill -9 spotifyd >/dev/null 2>&1
+	kill -15 spotifyd >/dev/null 2>&1
 	spotifyd
 	spt
 end
