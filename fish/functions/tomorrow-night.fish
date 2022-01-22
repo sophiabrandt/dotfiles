cef1ce3ca25c2aba42d530ca0e2139dae3719ed8
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2022-01-22 10:43:52 +0100

    👌 IMPROVE: Remove tomorrow-night

--- fish/functions/tomorrow-night.fish
+++
@@ -1,4 +0,0 @@
-function tomorrow-night
-	kitty @ set-colors -a -c ~/.config/kitty/themes/base16-tomorrow-night-256.conf
-	echo 'colorscheme base16-tomorrow-night' > ~/.vim/custom/colorscheme.vim
-end
