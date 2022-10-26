6be826a041f6bb693a68ebcdad66b23077977680
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2022-10-26 12:41:40 +0200

    👌 IMPROVE: Remove asdf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,6 @@ set -g -x fish_greeting ''
 # opam configuration
 source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 
-# asdf
-source ~/.asdf/asdf.fish
-
 # zoxide
 zoxide init fish | source
 
