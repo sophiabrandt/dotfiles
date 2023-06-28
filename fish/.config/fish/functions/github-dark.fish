31e116c7efa0ecfb239f99390be6dc9e2595957a
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-06-28 20:40:04 +0200

    👌 IMPROVE: Use different github theme

--- fish/.config/fish/functions/github-dark.fish
+++ fish/.config/fish/functions/github-dark.fish
@@ -1,6 +1,6 @@
 function github-dark
 	kitty +kitten themes --reload-in=all Github Dark
-	echo 'colorscheme github_dark_tritanopia' > ~/.vim/custom/colorscheme.vim
+	echo 'colorscheme github_dark' > ~/.vim/custom/colorscheme.vim
 	sed -e '1 c\
-theme = "github_dark_tritanopia"' ~/.config/helix/config.toml > ~/.config/helix/config.toml.tmp && mv ~/.config/helix/config.toml.tmp ~/.config/helix/config.toml
+theme = "github_dark"' ~/.config/helix/config.toml > ~/.config/helix/config.toml.tmp && mv ~/.config/helix/config.toml.tmp ~/.config/helix/config.toml
 end
