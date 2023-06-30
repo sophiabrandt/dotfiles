4ef47aada9f4ef810e2cbbba8d94ccaaca596738
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-06-30 16:17:42 +0200

    👌 IMPROVE: Use Github Dark Dimmed

--- fish/.config/fish/functions/github-dark.fish
+++ fish/.config/fish/functions/github-dark.fish
@@ -1,6 +1,6 @@
 function github-dark
-	kitty +kitten themes --reload-in=all Github Dark
-	echo 'colorscheme github_dark' > ~/.vim/custom/colorscheme.vim
+	kitty +kitten themes --reload-in=all Github Dark Dimmed
+	echo 'colorscheme github_dark_dimmed' > ~/.vim/custom/colorscheme.vim
 	sed -e '1 c\
-theme = "github_dark"' ~/.config/helix/config.toml > ~/.config/helix/config.toml.tmp && mv ~/.config/helix/config.toml.tmp ~/.config/helix/config.toml
+theme = "github_dark_dimmed"' ~/.config/helix/config.toml > ~/.config/helix/config.toml.tmp && mv ~/.config/helix/config.toml.tmp ~/.config/helix/config.toml
 end
