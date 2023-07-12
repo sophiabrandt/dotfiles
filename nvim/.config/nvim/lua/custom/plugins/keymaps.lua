5e1a747d12568385d48d96e6946e9b6725ca044c
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-07-12 18:16:13 +0200

    👌 IMPROVE: Align keymaps for formatting code

--- nvim/.config/nvim/lua/custom/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/custom/plugins/keymaps.lua
@@ -1,5 +1,5 @@
 return {
-	vim.keymap.set('n', '<localleader>f', vim.lsp.buf.format, { desc = '[F]ormat code' }),
+	vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, { desc = 'Format code' }),
 	vim.api.nvim_set_keymap('i', 'hsr', '=>', { silent = true }),
 	vim.api.nvim_set_keymap('i', 'nsr', '->', { silent = true })
 }
