defd007b242eaa2401d1037ca4e5c80d22820b8e
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-07-16 08:41:05 +0200

    👌 IMPROVE: Remove moving into notes directory

--- fish/.config/fish/functions/notes.fish
+++ fish/.config/fish/functions/notes.fish
@@ -1,10 +1,8 @@
 function notes
   set SUBJECT $argv[1]
   if test -z "$SUBJECT"
-    cd ~/notes
-    hx NOTES.md
+    hx ~/notes/NOTES.md
   else
-    cd ~/notes
     hx "$SUBJECT".md ~/notes/NOTES.md
   end
 end
