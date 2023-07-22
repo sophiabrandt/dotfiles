98f70be5d5c22a5c39507a4efc4d68cd4a18b8f3
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-07-22 21:19:19 +0200

    🐛 FIX: Close buffer instead of quitting

--- nvim/.config/nvim/lua/custom/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/custom/plugins/keymaps.lua
@@ -1,7 +1,7 @@
 return {
 	vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, { desc = 'Format code' }),
 	vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search [F]iles' }),
-	vim.api.nvim_set_keymap('n', '<leader>x', ':x<CR>', { silent = true, noremap = true }),
+	vim.api.nvim_set_keymap('n', '<leader>x', ':<C-u>bp<bar>bd! #<CR>', { silent = true, noremap = true }),
 	vim.api.nvim_set_keymap('i', 'hsr', '=>', { silent = true }),
 	vim.api.nvim_set_keymap('i', 'nsr', '->', { silent = true })
 }
