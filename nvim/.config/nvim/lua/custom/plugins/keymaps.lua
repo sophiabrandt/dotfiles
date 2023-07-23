b96662a3847d9e3465de9492536f2b7c6ebc0052
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-07-23 20:38:34 +0200

    📦 NEW: Use same insert macros for hashrocket (=>) and normal arrow (->) in Helix & NeoVim/Ideavim

--- nvim/.config/nvim/lua/custom/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/custom/plugins/keymaps.lua
@@ -3,5 +3,5 @@ return {
 	vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search [F]iles' }),
 	vim.api.nvim_set_keymap('n', '<leader>x', ':<C-u>bp<bar>bd! #<CR>', { silent = true, noremap = true }),
 	vim.api.nvim_set_keymap('i', 'hsr', '=>', { silent = true }),
-	vim.api.nvim_set_keymap('i', 'nsr', '->', { silent = true })
+	vim.api.nvim_set_keymap('i', 'nra', '->', { silent = true })
 }
