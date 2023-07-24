f41d18e879e0a57e9d2fd31b96a55813e81cf6fe
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-07-24 15:04:56 +0200

    👌 IMPROVE: noremap for insert key mappings

--- nvim/.config/nvim/lua/custom/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/custom/plugins/keymaps.lua
@@ -2,6 +2,6 @@ return {
 	vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, { desc = 'Format code' }),
 	vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search [F]iles' }),
 	vim.api.nvim_set_keymap('n', '<leader>x', ':<C-u>bp<bar>bd! #<CR>', { silent = true, noremap = true }),
-	vim.api.nvim_set_keymap('i', 'hsr', '=>', { silent = true }),
-	vim.api.nvim_set_keymap('i', 'nra', '->', { silent = true })
+	vim.api.nvim_set_keymap('i', 'hsr', '=>', { silent = true, noremap = true }),
+	vim.api.nvim_set_keymap('i', 'nra', '->', { silent = true, noremap = true })
 }
