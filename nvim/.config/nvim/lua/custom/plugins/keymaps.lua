726cb4de3fff43cecc369d489b6326b7ade34ab1
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-07-27 12:39:27 +0200

    📦 NEW: Tmux popup for Neovim with lazygit

--- nvim/.config/nvim/lua/custom/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/custom/plugins/keymaps.lua
@@ -3,5 +3,7 @@ return {
 	vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search [F]iles' }),
 	vim.api.nvim_set_keymap('n', '<leader>x', ':<C-u>bp<bar>bd! #<CR>', { silent = true, noremap = true }),
 	vim.api.nvim_set_keymap('i', 'hsr', '=>', { silent = true, noremap = true }),
-	vim.api.nvim_set_keymap('i', 'nra', '->', { silent = true, noremap = true })
+	vim.api.nvim_set_keymap('i', 'nra', '->', { silent = true, noremap = true }),
+	vim.api.nvim_set_keymap('n', '<C-g>', [[<Cmd>lua vim.fn.system("tmux popup -h '80%' -w '80%' -E 'lazygit'")<CR>]],
+		{ noremap = true, silent = true })
 }
