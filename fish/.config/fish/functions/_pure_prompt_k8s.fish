13a11ad4f4d1519e0f7eea56645c96a67fadc276
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-10-08 10:51:42 +0200

    👌 IMPROVE: Update fisher plugins

--- fish/.config/fish/functions/_pure_prompt_k8s.fish
+++ fish/.config/fish/functions/_pure_prompt_k8s.fish
@@ -2,9 +2,16 @@ function _pure_prompt_k8s
     if set --query pure_enable_k8s;
         and test "$pure_enable_k8s" = true;
         and _pure_check_availability pure_enable_k8s kubectl
+        and test -n (_pure_k8s_context) # todo: use $(cmd) syntax when Fish 3.3.1 is dropped
 
-        set -l context (_pure_set_color $pure_color_k8s_context)(_pure_k8s_context)
-        set -l namespace (_pure_set_color $pure_color_k8s_namespace)(_pure_k8s_namespace)
-        echo "$pure_symbol_k8s_prefix $context/$namespace"
+        set --local symbol (_pure_set_color $pure_color_k8s_prefix)$pure_symbol_k8s_prefix
+        set --local context (_pure_set_color $pure_color_k8s_context)(_pure_k8s_context)
+        set --local namespace (_pure_set_color $pure_color_k8s_namespace)(_pure_k8s_namespace)
+
+        if test -n "$namespace"
+            set --local namespace (_pure_set_color $pure_color_k8s_namespace)default
+        end
+
+        echo "$symbol $context/$namespace"
     end
 end
