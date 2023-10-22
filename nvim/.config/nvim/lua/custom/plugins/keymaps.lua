be67d3d0d380e6959519f745d7c0358e111385ab
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-10-22 16:25:36 +0200

    🐛 FIX: Remove shadowing key mappings

--- nvim/.config/nvim/lua/custom/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/custom/plugins/keymaps.lua
@@ -2,9 +2,10 @@ return {
 	vim.keymap.set('n', 'U', '<C-r>', { noremap = true }), -- better Undo
 	vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, { desc = 'Format code' }),
 	vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'Search [F]iles' }),
-	vim.api.nvim_set_keymap('n', '<leader>x', ':<C-u>bp<bar>bd! #<CR>', { silent = true, noremap = true }),
-	vim.api.nvim_set_keymap('n', 'gp', ':bprevious<CR>', { silent = true, noremap = true }),
-	vim.api.nvim_set_keymap('n', 'gn', ':bnext<CR>', { silent = true, noremap = true }),
+	vim.api.nvim_set_keymap('n', '<leader>x', ':<C-u>bp<bar>bd! #<CR>',
+		{ silent = true, noremap = true, desc = "Close buffer" }),
+	vim.api.nvim_set_keymap('n', '[b', ':bprevious<CR>', { silent = true, noremap = true, desc = "Previous buffer" }),
+	vim.api.nvim_set_keymap('n', ']b', ':bnext<CR>', { silent = true, noremap = true, desc = "Next buffer" }),
 	vim.api.nvim_set_keymap('n', '<localleader>g',
 		[[<Cmd>lua vim.fn.system("tmux popup -h '80%' -w '80%' -E 'lazygit'")<CR>]],
 		{ noremap = true, silent = true }),
