a709020d3368b1227863f58e71f457edb293eac4
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-11-10 15:45:40 +0100

    👌 IMPROVE: Rollback to neovim as default term editor

--- fish/.config/fish/functions/notes.fish
+++ fish/.config/fish/functions/notes.fish
@@ -1,8 +1,8 @@
 function notes
   set SUBJECT $argv[1]
   if test -z "$SUBJECT"
-    hx ~/notes/NOTES.md
+    nvim ~/notes/NOTES.md
   else
-    hx "$SUBJECT".md ~/notes/NOTES.md
+    nvim "$SUBJECT".md ~/notes/NOTES.md
   end
 end
