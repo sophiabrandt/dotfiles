fdf324414200c07c7a635aa98a94575b937ec29d
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-11-11 09:28:32 +0100

    📦 NEW: copilot.lua, copilot-cmp, oil.nvim

---
+++ nvim/.config/nvim/lua/custom/plugins/oil.lua
@@ -0,0 +1,8 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+}
+
