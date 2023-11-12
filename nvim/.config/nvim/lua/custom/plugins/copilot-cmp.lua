d886a4e7b044021642c2102120fb26733b5915c2
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-11-12 14:12:24 +0100

    👌 IMPROVE: better structure copilot-cmp lua

--- nvim/.config/nvim/lua/custom/plugins/copilot-cmp.lua
+++ nvim/.config/nvim/lua/custom/plugins/copilot-cmp.lua
@@ -1,6 +1,28 @@
+local cmp = require('cmp')
+
+local has_words_before = function()
+	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+
+-- Return the plugin configuration
 return {
 	"zbirenbaum/copilot-cmp",
 	config = function()
 		require("copilot_cmp").setup()
+
+		-- Additional cmp configuration specific to copilot-cmp
+		cmp.setup({
+			mapping = {
+				["<Tab>"] = vim.schedule_wrap(function(fallback)
+					if cmp.visible() and has_words_before() then
+						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+					else
+						fallback()
+					end
+				end),
+			},
+		})
 	end
 }
