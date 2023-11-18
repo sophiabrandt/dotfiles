cba57d070840b899d23831c4f10a1613d2af49e7
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-11-18 10:37:27 +0100

    📦 NEW: Add language servers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -510,11 +510,13 @@ require('mason-lspconfig').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- csharp_ls = {},
   -- rust_analyzer = {},
-  -- tsserver = {},
+  tsserver = {},
+  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  angularls = {},
 
   lua_ls = {
     Lua = {
