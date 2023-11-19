74c54e77086518e27c5df9e16b037c1f90de34ee
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2023-11-19 15:21:14 +0100

    👌 IMPROVE: Remove default servers, but keep efm
    efm needs to be installed for efmls to work. Rest can be installed
    manually with Mason.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -515,13 +515,12 @@ require('mason-lspconfig').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  gopls = {},
+  -- gopls = {},
   -- pyright = {},
   -- csharp_ls = {},
   -- rust_analyzer = {},
-  tsserver = {},
+  -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-  angularls = {},
   efm = {},
 
   lua_ls = {
