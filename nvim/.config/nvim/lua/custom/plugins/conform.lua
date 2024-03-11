b47c3bbee9ae0bc3be376a4512cb1f9e26cdd2fd
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2024-03-11 20:44:23 +0100

    📦 NEW: Add toggle function for formatting

--- nvim/.config/nvim/lua/custom/plugins/conform.lua
+++ nvim/.config/nvim/lua/custom/plugins/conform.lua
@@ -23,7 +23,13 @@ return {
       python = { 'isort', 'black' },
     },
     -- Set up format-on-save
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_fallback = true }
+    end,
     -- Customize formatters
     formatters = {
       shfmt = {
@@ -32,6 +38,23 @@ return {
     },
   },
   init = function()
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
     -- If you want the formatexpr, here is the place to set it
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   end,
