918c93ac4e02ddb9b050370d81195e7821360c39
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2024-07-08 17:44:55 +0200

    📦 NEW: new keymaps for insert mode

--- nvim/.config/nvim/lua/custom/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/custom/plugins/keymaps.lua
@@ -1,11 +1,13 @@
 return {
-  vim.keymap.set('n', 'U', '<C-r>', { noremap = true }), -- better Undo
-  vim.keymap.set(
+  vim.api.nvim_set_keymap('n', 'U', '<C-r>', { noremap = true }), -- better Undo
+  vim.api.nvim_set_keymap(
     'n',
     '<leader>f',
     "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
     { noremap = true, desc = 'Search [F]iles' }
   ),
+  vim.api.nvim_set_keymap('i', '<c-e>', 'c-o>$', { silent = true, noremap = true, desc = 'Jump to end of line' }),
+  vim.api.nvim_set_keymap('i', '<c-a>', 'c-o>^', { silent = true, noremap = true, desc = 'Jump to start of line' }),
   vim.api.nvim_set_keymap('n', '<leader>x', ':<C-u>bp<bar>bd! #<CR>', { silent = true, noremap = true, desc = 'Close buffer' }),
   vim.api.nvim_set_keymap('n', '<leader>X', ':<C-u>%bd<bar>e#<bar>bd#<CR>', { silent = true, noremap = true, desc = 'Close all buffers except current one' }),
   vim.api.nvim_set_keymap('n', '[b', ':bprevious<CR>', { silent = true, noremap = true, desc = 'Previous buffer' }),
