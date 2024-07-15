c29023c2ec59a6daf329ddebe7312c195bc9e94b
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2024-07-15 11:36:24 +0200

    👌 IMPROVE: Use go-revive & gofumpt for Go

--- nvim/.config/nvim/lua/custom/plugins/efmls-config.lua
+++ nvim/.config/nvim/lua/custom/plugins/efmls-config.lua
@@ -4,6 +4,13 @@ return {
   requires = { 'neovim/nvim-lspconfig' },
   config = function()
     local languages = require('efmls-configs.defaults').languages()
+    languages = vim.tbl_extend('force', languages, {
+      -- you custom languages, or overrides
+      go = {
+        require 'efmls-configs.linters.go_revive',
+        require 'efmls-configs.formatters.gofumpt',
+      },
+    })
     require('lspconfig').efm.setup {
       filetypes = vim.tbl_keys(languages),
       settings = {
