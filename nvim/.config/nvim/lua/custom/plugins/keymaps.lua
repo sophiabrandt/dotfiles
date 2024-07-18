2c09dfce8acb5cafc782dee628bef0c7e21427a2
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2024-07-18 21:19:11 +0200

    🐛 FIX: keymappings

--- nvim/.config/nvim/lua/custom/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/custom/plugins/keymaps.lua
@@ -6,8 +6,8 @@ return {
     "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
     { noremap = true, desc = 'Search [F]iles' }
   ),
-  vim.api.nvim_set_keymap('i', '<c-e>', 'c-o>$', { silent = true, noremap = true, desc = 'Jump to end of line' }),
-  vim.api.nvim_set_keymap('i', '<c-a>', 'c-o>^', { silent = true, noremap = true, desc = 'Jump to start of line' }),
+  vim.api.nvim_set_keymap('i', '<c-e>', '<c-o>$', { silent = true, noremap = true, desc = 'Jump to end of line' }),
+  vim.api.nvim_set_keymap('i', '<c-a>', '<c-o>^', { silent = true, noremap = true, desc = 'Jump to start of line' }),
   vim.api.nvim_set_keymap('n', '<leader>x', ':<C-u>bp<bar>bd! #<CR>', { silent = true, noremap = true, desc = 'Close buffer' }),
   vim.api.nvim_set_keymap('n', '<leader>X', ':<C-u>%bd<bar>e#<bar>bd#<CR>', { silent = true, noremap = true, desc = 'Close all buffers except current one' }),
   vim.api.nvim_set_keymap('n', '[b', ':bprevious<CR>', { silent = true, noremap = true, desc = 'Previous buffer' }),
