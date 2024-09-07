5b5711596279ccd0b28cc564bfa5b24fc9d19f76
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2024-09-07 10:11:57 +0200

    🐛 FIX: brew info

--- fish/.config/fish/functions/brinfo.fish
+++ fish/.config/fish/functions/brinfo.fish
@@ -1,3 +1,5 @@
 function brinfo --description 'fuzzy brew info'
-    brew info --json --eval-all \E | jq -r '.[].name' \E | fzf --cycle --tmux --preview 'brew info {}' --color bg:#222222,preview-bg:#333333 --info=inline-right --ellipsis=… --tabstop=4 --highlight-line
+    brew info --json --eval-all \
+  | jq -r '.[].name' \
+  | fzf --cycle --tmux --preview 'brew info {}' --color bg:#222222,preview-bg:#333333 --info=inline-right --ellipsis=… --tabstop=4 --highlight-line 
 end
